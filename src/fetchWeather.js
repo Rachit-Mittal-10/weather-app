676f2b2157572880462046b9ccd22976d8c93d6e
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-10-16 22:54:01 +0530

    Refactored Code

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,10 +1,23 @@
-export default function Weather(){
+/*
+getWeather is the function that is responsible for fetching the Weather Detail for a location from API(openWeathermap.org)
+*/
+let getWeather = function Weather(){
     const API_KEY = "e3ff4bc0e67ef796cf6f736a27e81558";
     return async function fetchWeather(location){
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}`,{mode:'cors'});
         const weatherData = await response.json();
         return weatherData;
     }
-};
-
-
+}();
+/*
+This function will showcase the weather to html page
+*/
+export default async function showWeather(location){
+    try{
+        const weatherData = await getWeather(location);
+        console.log(weatherData);
+    }
+    catch(error){
+        console.error("Error fetching the Weather data: ", error);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import Weather from "./fetchWeather";
+import getWeather from "./fetchWeather";
 
-async function getWeather(location){
-    try{
-        const weatherData = await Weather()(location);
-        console.log(weatherData.city);
-    }
-    catch(error){
-        console.error("Error fetching the Weather data: ", error);
-    }
-}
-
-getWeather('delhi');
+getWeather('Delhi');
